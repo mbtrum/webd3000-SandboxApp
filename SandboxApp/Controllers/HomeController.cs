2b4c31bb23d398c24a43aedfb5b2ea962813f55f
Michael Trumbull <michael.trumbull@nscc.ca>
2026-01-13 14:29:49 -0400

    Learning controllers and routing.

--- SandboxApp/Controllers/HomeController.cs
+++ SandboxApp/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SandboxApp.Models;
@@ -6,12 +7,52 @@ namespace SandboxApp.Controllers
 {
     public class HomeController : Controller
     {
+        // constructor
+        public HomeController()
+        {            
+        }
+
+        public IActionResult Unauth()
+        {
+            bool isLoggedIn = true;
+
+            if (isLoggedIn)
+            {
+                return View();
+            }
+            else
+            {
+                return new UnauthorizedResult();
+            }                
+        }
+
+        public JsonResult Json()
+        {
+            return Json(new List<string> { "Wow,", "this", "is", "crazy!" });
+        }
+
+        public ContentResult PlainText()
+        {
+            return Content("Hello, this is plain text!");
+        }
+
+
         public IActionResult Index()
+        {
+            return View();
+        }               
+
+        public ViewResult Privacy()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Pikachu()
+        {
+            return View();
+        }
+
+        public IActionResult Ditto()
         {
             return View();
         }
